85e9f6bbc61e266f8d61beea728f83447d4ab4d8
JelsinPalomino <jstpalomino@hotmail.com>
2025-04-18 21:55:11 -0500

    [code] We add the qget() function in utils.py

---
+++ wbgapi/utils.py
@@ -0,0 +1,31 @@
+import re
+
+def qget(q):
+    '''Returns the lower-case search string from text along with possible options. This is used internally
+
+       None or empty string returns None
+
+       A leading '!' is interpreted as a request to search the complete string. In most cases
+       this is ignored, but for series, it includes the ending parenethical part of the search which
+       is omitted by default.
+
+    Examples:
+        q, type = qget()
+        q,_     = qget()        # if you don't care about the flag
+        q       = qget()[0]        # likewise
+
+    Notes:
+        If full search doesn't apply, you should ignore the flag returned from qget and pass the default
+        option to qmatch. This makes for efficient searching.
+    '''
+
+    if q:
+        q = q.lower()
+        if q[0] == '!':
+            return q[1:], True
+    
+        return q, False
+    
+    return None, False
+
+qmatch_expr = None
